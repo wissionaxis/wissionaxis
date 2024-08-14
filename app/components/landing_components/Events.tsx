c2d628d326480952e728bbdd9b803ad0dc870f12
SiddharthaArepalli <asiddharthagoud@gmail.com>
2024-08-15 00:17:44 +0530

    added events carousal
    added events carousal

--- app/components/landing_components/Events.tsx
+++ app/components/landing_components/Events.tsx
@@ -5,6 +5,7 @@ import event2 from '@/app/assets/images/event2.jpg';
 import event3 from '@/app/assets/images/event3.jpg';
 import event4 from '@/app/assets/images/event2.jpg';
 
+
 const events = [
   { id: 1, image: event1, title: 'Workshop on Example 101' },
   { id: 2, image: event2, title: 'Workshop on Example 101' },
@@ -16,7 +17,7 @@ const OurEvents = () => {
   return (
     <section className="py-16 px-10">
       <h2 className="text-center text-4xl font-bold mb-8">Our Events</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 animate-scroll">
         {events.map(event => (
           <div key={event.id} className="bg-white rounded-md p-4 h-[15rem] flex-colm shadow-lg">
             <Image src={event.image} alt={event.title} className="w-full h-32 object-cover rounded-t-md" />
@@ -29,3 +30,10 @@ const OurEvents = () => {
 };
 
 export default OurEvents;
+
+
+
+
+
+
+
