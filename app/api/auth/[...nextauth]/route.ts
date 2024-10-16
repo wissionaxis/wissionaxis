8ccf833b56d6e0044b5ca6a6c6f75f1b3376a265
Thanuja <135441914+klthanuja@users.noreply.github.com>
2024-10-16 15:33:29 +0530

    Final push for backend ..need to check for bugs and stuff

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -21,8 +21,8 @@ interface AuthUser {
 const Options: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId:'496438207267-5sm1joa903t9k6ddgv5ulaigq8qvql46.apps.googleusercontent.com',
-      clientSecret:'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' ,
+      clientId:'GOOGLE_CLIENT_ID',
+      clientSecret:'GOOGLE_CLIENT_SECRET' ,
     }),
     CredentialsProvider({
       name: 'Credentials',
